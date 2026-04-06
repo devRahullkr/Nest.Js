deddfc0a2a4166f2e400ae8a7513dd09448c5fce
Rahulllkr <rahulkumar8604745@gmail.com>
2026-04-06 16:50:41 +0530

    add user

--- backend-project/src/auth/auth.service.ts
+++ backend-project/src/auth/auth.service.ts
@@ -1,11 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  async signup(data:any){
-    
+  async signup(data: any) {
+    const userExist = await this.prisma.user.findUnique({
+      where: { email: data.email },
+    });
+
+    if (userExist) {
+      throw new BadRequestException('User already exists');
+    }
+
+    const hashed = await bcrypt.hash(data.password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: data.email,
+        password: hashed,
+      },
+    });
+
+    return {
+      message: 'signup successfully',
+      user,
+    };
   }
 }
